63e274c8cd57af2e86fc3bc5bd2992dd7d096617
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-05 18:28:35 -0700

    refactor methods

--- lib/leet.rb
+++ lib/leet.rb
@@ -1,18 +1,13 @@
 require 'pry'
 
-def leetspeak(phrase)
+def leet_speak(phrase)
   phrase_split = phrase.split(" ").map do |word|
-    if word[0] == "s"
-      first_s = word.slice!(0)
-      first_s + word_leet(word)
-    else
-      word_leet(word)
-    end
+    word[0] == "s" ? word.slice!(0) + leet_word(word) : leet_word(word)
   end
   phrase_split.join(" ")
 end
 
-def word_leet(word)
+def leet_word(word)
   leet_rules = { 'e' => 3, 'o' => 0, "I" => 1, "s" => "z"}
-  word.chars.map{ |letter| leet_rules.key?(letter) ? leet_rules[letter] : letter}.join
+  word.chars.map{ |letter| leet_rules[letter] || letter}.join
 end
